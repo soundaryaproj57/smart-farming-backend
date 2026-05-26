a00ff0766db718ec7bec5e9dd387fb6751898bf7
Soundarya <soundarya.proj@gmail.com>
2026-05-26 12:30:48 +0530

    safe firebase config

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,29 @@
-import firebase_admin
-from firebase_admin import credentials, db
+try:
+    import firebase_admin
+    from firebase_admin import credentials, db
 
-cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate(
+        "firebase_key.json"
+    )
 
-firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://smart-farming-130fe-default-rtdb.asia-southeast1.firebasedatabase.app/"
-})
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(
+            cred,
+            {
+                "databaseURL":
+                "https://smart-farming-130fe-default-rtdb.asia-southeast1.firebasedatabase.app/"
+            }
+        )
 
-rtdb = db
-#import firebase_admin
-#from firebase_admin import credentials, firestore
+    rtdb = db
 
-#cred = credentials.Certificate("firebase_key.json")
-#firebase_admin.initialize_app(cred)
+    print("🔥 Firebase Connected")
 
-#db = firestore.client()
+except Exception as e:
+
+    print(
+        "❌ Firebase Disabled:",
+        e
+    )
+
+    rtdb = None
